848b0b53c6e668b7d0dcae8f8b772b17f53a7a74
Passing-of-A-Dream <986642549@qq.com>
2022-06-14 08:41:37 +0800

    commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Index from './pages/index';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Index />
+      {/* <Select /> */}
+    </div>
+  );
+}
+
+export default App;
